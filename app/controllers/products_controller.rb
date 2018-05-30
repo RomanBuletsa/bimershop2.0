63f7cc9ca802d5b9c3f80313c150823b528c66de
kevyn911 <advancerp13@gmail.com>
2018-05-31 02:12:59 +0300

    Add some desing in used cars page + fix controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,32 +1,39 @@
 class ProductsController < ApplicationController
 before_action :authenticate_user!, except: [:show]
+before_action :set_product, only: [:edit, :update, :destroy, :show]
 
   def index
-  	@products = Product.all
+      @products = Product.all
   end
 
   def show
   end
-  
+
   def new
     @product = Product.new
   end
-  
+
   def create
     @product = current_user.products.build(products_params)
     if @product.save
       redirect_to root_path
-    end   
+    end
   end
-  
+
   def edit
   end
-  
+
   def update
   end
-  
+
 private
   def products_params
     params.require(:product).permit(:user_id, :name, :description, :price, :count, {icons: []})
   end
+def set_product
+@product = Product.find(params[:id])
+end
+
+
+
 end
