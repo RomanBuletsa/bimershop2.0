741e6ca6431667144325e1b7429d43916d2e1588
thunder99 <abba13517@gmail.com>
2018-05-31 00:57:23 +0300

    testing model post

--- app/models/postcomment.rb
+++ app/models/postcomment.rb
@@ -1,4 +1,7 @@
 class Postcomment < ApplicationRecord
 	belongs_to :user
 	belongs_to :post
+
+	validates :text, length: { in: 1..25}
+	validates :text, presence: true
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   mount_uploaders :icons, IconUploader
   serialize :icons, JSON
-  
+
   belongs_to :user
+  validates :name, :price, :count, presence: true
 end
